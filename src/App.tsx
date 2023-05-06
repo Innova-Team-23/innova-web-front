891b9c72b284ab31b013c269aeac5896d5bede87
Rindra Nantenaina <124867262+Rindra11@users.noreply.github.com>
2023-05-06 19:35:18 +0300

    navbar home

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import './App.css';
 import Navbar from './View/Navbar/Navbar';
+import Home from './View/Login/Home';
 
 function App() {
   return (
     <div>
       <Navbar/>
+      <Home/>
     </div>
   );
 }
