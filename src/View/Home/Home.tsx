a71236b783dc0c254f23c044ec45f71184d37df1
Ravaka15 <ravakiniainatokyhary@gmail.com>
2023-05-07 07:55:41 +0300

    Home amelioration

--- src/View/Home/Home.tsx
+++ src/View/Home/Home.tsx
@@ -1,60 +1,77 @@
-import React from 'react'
+import React from "react";
 import { Link } from "react-router-dom";
-import '../../Assets/style/Home.css';
+import "../../Assets/style/Home.css";
 
 const Home: React.FC = () => {
-
-  return(
-    <div className='homes'>
-        
-        
-    <section className="home" id="home">
-    <div className="home__img">
+  return (
+    <div className="homes">
+      <section className="home" id="home">
+        <div className="background hidden">
+          <div className="home__img"></div>
         </div>
         <div className="content">
-            <h3>Bonjour, Bienvenue</h3>
-            <span>Dans notre site de préduction de rêve..</span>
-            <p>Le rêve est une « disposition de l'esprit généralement nocturne, survenant au cours du sommeil, et 
-              qui procure à l'individu éveillé des souvenirs nommés eux aussi rêves ».</p>
-            <Link to="/" className="btn">Explorer maintenant</Link>
-            
+          <h3> Bienvenue chez Onirix </h3>
+          <span>Dans notre site de préduction de rêve..</span>
+          <p>
+            Le rêve est une « disposition de l'esprit généralement nocturne,
+            survenant au cours du sommeil, et qui procure à l'individu éveillé
+            des souvenirs nommés eux aussi rêves ».
+          </p>
+          <Link to="/" className="btn">
+            Commencer
+          </Link>
         </div>
-    </section>
+      </section>
 
-    <section className="about" id="about">
+      <section className="about" id="about">
         <h1 className="heading">A propos</h1>
         <div className="row">
-            <div className="video-container">
-                <h3>Meilleur video</h3>
-            </div>
-            <div className="content">
-                <h3>Pourquoi nous choisir?</h3>
-                <p>Au cours de l'histoire et des civilisations, le rêve a été un moyen de s'affranchir du temps et de l'espace ordinaires, pour accéder au surnaturel, aux ancêtres, au divin, ou encore comme un moyen de guérison, de connaissance et de révélation.</p>
-                <p>L'approche rationnelle et scientifique en fait un processus ancré dans le corps et lié à l'activité cérébrale au cours du sommeil. Le rêve pose toujours la question de son sens et de sa signification, ou de son rôle et de sa fonction (approches philosophique, psychodynamique, neurobiologique…).</p>
-                <Link to="/" className="btn">Apprendre mieux</Link>
-            </div>
+          <div className="video-container">
+            <h3>Meilleur video</h3>
+          </div>
+          <div className="content">
+            <h3>Pourquoi nous choisir?</h3>
+            <p>
+              Au cours de l'histoire et des civilisations, le rêve a été un
+              moyen de s'affranchir du temps et de l'espace ordinaires, pour
+              accéder au surnaturel, aux ancêtres, au divin, ou encore comme un
+              moyen de guérison, de connaissance et de révélation.
+            </p>
+            <p>
+              L'approche rationnelle et scientifique en fait un processus ancré
+              dans le corps et lié à l'activité cérébrale au cours du sommeil.
+              Le rêve pose toujours la question de son sens et de sa
+              signification, ou de son rôle et de sa fonction (approches
+              philosophique, psychodynamique, neurobiologique…).
+            </p>
+            <Link to="/" className="btn">
+              Apprendre mieux
+            </Link>
+          </div>
         </div>
-    </section>
+      </section>
 
-    <section className="card" id="card">
-    <h1 className="heading">Poster Vos rêves</h1>
-    <div className="box-container">
-        <div className="box">
+      <section className="card" id="card">
+        <h1 className="heading">Poster Vos rêves</h1>
+        <div className="box-container">
+          <div className="box">
             <div className="content">
-            <Link to="/Historique"><h3>De votre cauchemar...</h3></Link>      
+              <Link to="/Historique">
+                <h3>De votre cauchemar...</h3>
+              </Link>
             </div>
-        </div>
-        <div className="box">
+          </div>
+          <div className="box">
             <div className="content">
-                <Link to="/Historique"><h3>De votre peur..</h3></Link>
+              <Link to="/Historique">
+                <h3>De votre peur..</h3>
+              </Link>
             </div>
+          </div>
         </div>
+      </section>
     </div>
-</section>
-
-    </div>
-  )
-
-}
+  );
+};
 
 export default Home;
